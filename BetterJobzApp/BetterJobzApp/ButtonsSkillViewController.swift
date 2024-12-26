0645648eefd3a198e9483f4953cee7132c23c126
FatimaMalalla <FatimaMallala.04@gmail.com>
2024-12-26 14:50:20 +0300

    skills development addition

---
+++ BetterJobzApp/BetterJobzApp/ButtonsSkillViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ButtonsSkillViewController.swift
+//  BetterJobzApp
+//
+//  Created by Guest User on 26/12/2024.
+//
+
+import UIKit
+
+class ButtonsSkillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backToDash(_ sender: Any){
+        self.dismiss(animated: true, completion: nil)
+        
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
